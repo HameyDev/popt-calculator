5c0a91f7f9ea59c9a0822fe3837f846acf06590a
M HAMMAD ZULFIQAR <mhammad.zulfiqar.se@gmail.com>
2025-10-14 18:28:28 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -221,7 +221,7 @@ export default function IWICalculator() {
       {/* Report Section */}
       <div
         ref={reportRef}
-        className="mt-10 bg-white p-6 rounded shadow overflow-x-auto"
+        className="mt-10 bg-white p-6 rounded shadow overflow-x-auto text-[14px] leading-tight font-[Calibri]"
       >
         <h1 className="text-xl font-bold text-orange-600 mb-4">IWI</h1>
 
