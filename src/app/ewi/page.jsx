df5e13254dd681a831213248f506f85c537df351
M HAMMAD ZULFIQAR <mhammad.zulfiqar.se@gmail.com>
2025-10-18 15:01:44 +0500

    Update page.jsx

--- src/app/ewi/page.jsx
+++ src/app/ewi/page.jsx
@@ -230,7 +230,7 @@ export default function EWICalculator() {
       {/* Add Wall Button */}
       <button
         onClick={addWall}
-        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-purple-700 w-full sm:w-auto"
+        className="mt-4 px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700 w-full sm:w-auto"
       >
         + Add Wall
       </button>
