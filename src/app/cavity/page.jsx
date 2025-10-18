77d4baa49fbc32da7853c5a6f3d71f55ecca759a
M HAMMAD ZULFIQAR <mhammad.zulfiqar.se@gmail.com>
2025-10-18 14:48:55 +0500

    Update page.jsx

--- src/app/cavity/page.jsx
+++ src/app/cavity/page.jsx
@@ -279,7 +279,7 @@ export default function CWICalculator() {
            Total Solid Wall Area={totals.solidArea}m²
          </p>
         )}
-        <p className="mt-3 font-semibold text-green-500 border-2 border-green-500 px-2 py-1 w-48">
+        <p className="mt-3 font-semibold text-green-500 border-2 border-green-500 px-2 py-1 w-44">
           Insulated CWI Wall Area={totals.insulatedArea}m²
         </p>
         <p className="mt-3 font-semibold text-green-500">
